61728b64746f474118a789c1248937e73da43c4f
pappi <pappi@pappis-MacBook-Air.local>
2025-01-03 16:59:05 +0800

    Add Jest testing setup with Next.js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  setupFiles: ['<rootDir>/jest.setup.js'],
+};
